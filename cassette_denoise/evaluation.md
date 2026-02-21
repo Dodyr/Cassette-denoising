5f0b0e8f7ec8144b1568f294931ad463ea80ad67
Dodyr <misha.arapov@gmail.com>
2026-02-21 20:57:53 +0100

    evaluation

---
+++ cassette_denoise/evaluation.md
@@ -0,0 +1,9 @@
+AhRep
+Metric  Original Noisy           Model           Dolby
+       PEAQ       -2.817579       -1.319537       -2.194221
+  PEAQbasic       -2.252227       -0.944653       -2.085987
+SI-SDR (dB) tensor(40.8051) tensor(25.0254) tensor(27.7834)
+   SNR (dB) tensor(40.7940) tensor(25.0389) tensor(27.7062)
+        LSD       16.867521        7.191132       12.878913
+
+DS
--- model/model.py
+++ model/model.py
@@ -4,104 +4,88 @@ import librosa
 import soundfile
 import scipy
 from data_tools.utils import scaled_in
+
 class Model():
-    def __init__(self, model, weights, model_name, sample_rate, min_duration, frame_length, hop_length_frame, n_fft, hop_length_fft):
+    def __init__(self, model, sample_rate, min_duration, n_fft, hop_length, chunk_length):
         self.model = model
-        self.weights = weights
-        self.model_name = model_name
         self.sample_rate = sample_rate
         self.min_duration = min_duration
-        self.frame_length = frame_length
-        self.hop_length_frame = hop_length_frame
         self.n_fft = n_fft
-        self.hop_length_fft = hop_length_fft
-
-    def _process_single_channel(self, audio_1d):
-        # STFT
-        stft_noisy = librosa.stft(audio_1d, n_fft=self.n_fft, hop_length=self.hop_length_fft)
-        mag_noisy, phase_noisy = librosa.magphase(stft_noisy)
+        self.hop_length = hop_length
+        self.chunk_length = chunk_length
         
-        # dB -> scale
+    def process_mono(self, audio):
+        stft_noisy = librosa.stft(audio, n_fft=self.n_fft, hop_length=self.hop_length)
+        mag_noisy, phase = librosa.magphase(stft_noisy)
+        
         mag_noisy_db = librosa.amplitude_to_db(mag_noisy, ref=np.max)
-        X_input = scaled_in(mag_noisy_db)
+        input = scaled_in(mag_noisy_db)
         
-        n_bins, n_frames = X_input.shape
-        chunk_width = 512 
-         
-        # padding logic
-        pad_width = chunk_width - (n_frames % chunk_width)
-        if pad_width < chunk_width:
-             X_input = np.pad(X_input, ((0, 0), (0, pad_width)), mode='constant')
-             mag_noisy = np.pad(mag_noisy, ((0, 0), (0, pad_width)), mode='constant')
-             phase_noisy = np.pad(phase_noisy, ((0, 0), (0, pad_width)), mode='constant')
+        n_bins, n_frames = input.shape
         
-        n_frames_padded = X_input.shape[1]
-        num_chunks = n_frames_padded // chunk_width
-        
-        full_mask = np.zeros(X_input.shape)
-        
-        # prediction Loop
-        for i in range(num_chunks):
-            start = i * chunk_width
-            end = start + chunk_width
+        # padding
+        pad_width = self.chunk_length - (n_frames % self.chunk_length)
+        if pad_width < self.chunk_length:
+            input = np.pad(input, ((0, 0), (0, pad_width)), mode='constant')
+            mag_noisy = np.pad(mag_noisy, ((0, 0), (0, pad_width)), mode='constant')
+            phase = np.pad(phase, ((0, 0), (0, pad_width)), mode='constant')
             
-            chunk = X_input[:, start:end]
-            chunk = chunk.reshape(1, n_bins, chunk_width, 1)
+        n_padded = input.shape[1]
+        num_chunks = n_padded // self.chunk_length
+        
+        mask = np.zeros(input.shape)
+        
+        for i in range(num_chunks):
+            start = i * self.chunk_length
+            end = start + self.chunk_length
+            
+            chunk = input[:, start: end]
+            chunk = chunk.reshape(1, n_bins, self.chunk_length, 1)
             
             pred_mask = self.model.predict(chunk, verbose=0)
-            full_mask[:, start:end] = pred_mask[0, :, :, 0]
+            mask[:, start: end] = pred_mask[0, :, :, 0]
             
-        # apply mask
-        mag_clean = mag_noisy * full_mask
-        stft_clean = mag_clean * phase_noisy
+        mag_clean = mag_noisy * mask
+        stft_clean = mag_clean * phase
         
-        audio_clean = librosa.istft(stft_clean, hop_length=self.hop_length_fft)
+        audio_clean = librosa.istft(stft_clean, hop_length=self.hop_length)
         
         # trim padding
-        original_len = len(audio_1d)
-        if len(audio_clean) > original_len:
-             audio_clean = audio_clean[:original_len]
-        elif len(audio_clean) < original_len:
-             audio_clean = np.pad(audio_clean, (0, original_len - len(audio_clean)))            
+        if len(audio_clean) > len(audio):
+            audio_clean = audio_clean[: len(audio)]
+        elif len(audio_clean) < len(audio):
+            audio = np.pad(audio_clean, (0, len(audio) - len(audio_clean)))
             
         return audio_clean
-
-    def predict(self, artifacts_path, audio_file, output_file, noise_coef=1.0, headroom=1.0):
+    
+    def predict(self, artifacts_path, audio_file, output_file, headroom=1.0):
         full_path = os.path.join(artifacts_path, audio_file)
-        print(f"Processing: {full_path}")
         
-        # load audio
-        y, sr = librosa.load(full_path, sr=self.sample_rate, mono=False)
+        audio, sr = librosa.load(full_path, sr=self.sample_rate, mono=False)
         
-        final_audio = None
+        clean_audio = None
         
-        if y.ndim == 1:
+        if audio.ndim == 1:
             # mono
-            print("Mode: Mono")
-            final_audio = self._process_single_channel(y)
+            print("Mono")
+            clean_audio = self.process_mono(audio)
         else:
             # stereo
-            print("Mode: Stereo (Processing Left and Right channels separately)")
-            left_channel = y[0]
-            right_channel = y[1]
+            print("Stereo")
+            left_channel = audio[0]
+            right_channel = audio[1]
             
-            print("-> Processing Left Channel...")
-            clean_left = self._process_single_channel(left_channel)
+            clean_left = self.process_mono(left_channel)
+            clean_right = self.process_mono(right_channel)
             
-            print("-> Processing Right Channel...")
-            clean_right = self._process_single_channel(right_channel)
-            
-            final_audio = np.vstack([clean_left, clean_right]).T
-        
-        # apply headroom
+            clean_audio = np.vstack([clean_left, clean_right]).T
+
         if headroom != 1.0:
-            final_audio = final_audio * headroom
-        
-        print(f"Output audio shape: {final_audio.shape}")
-        
+            clean_audio = clean_audio * headroom
+            
         output_path = os.path.join(artifacts_path, output_file)
-        soundfile.write(output_path, final_audio, self.sample_rate)
-        print(f"Saved to: {output_path}")
+        soundfile.write(output_path, clean_audio, self.sample_rate)
+        print(f"Denoised and saved as {output_file}")
         
-        return final_audio
-    
+            
+        
